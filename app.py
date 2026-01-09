30b92bdb30a384c7dc83e723cdad66101f84ca08
kurokishi <kuroreidho@gmail.com>
2026-01-09 11:57:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,29 @@
+import os
 import streamlit as st
-from database import engine, init_db
+from database import init_db, engine
 from auth import login
 from permissions import allow
 from modules import tiket, approval, ekinerja
 
+os.makedirs("uploads", exist_ok=True)
+
 st.set_page_config("SIM-IT RSUD", layout="wide")
+
 init_db()
 
 if "user" not in st.session_state:
     login(engine)
     st.stop()
 
-menu = st.sidebar.radio("Menu",[
-    "Input Tiket",
-    "Approval",
-    "e-Kinerja"
-])
+menu = st.sidebar.radio("Menu", ["Input Tiket", "Approval", "e-Kinerja"])
 
-if menu=="Input Tiket":
-    allow("IT Support","Admin")
+if menu == "Input Tiket":
+    allow("Admin", "IT Support")
     tiket.page(engine)
 
-elif menu=="Approval":
+elif menu == "Approval":
+    allow("Admin", "Atasan")
     approval.page(engine)
 
-elif menu=="e-Kinerja":
+elif menu == "e-Kinerja":
     ekinerja.page(engine)
