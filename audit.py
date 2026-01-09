dd141cded315f6abdd053f01a45295bf690383b6
kurokishi <kuroreidho@gmail.com>
2026-01-09 11:18:44 +0700

    Add logging function for audit trail

---
+++ audit.py
@@ -0,0 +1,15 @@
+from sqlalchemy import text
+from datetime import datetime
+
+def log(engine, user, role, aksi, objek):
+    engine.execute(text("""
+    INSERT INTO audit_log
+    (user, role, aksi, objek, timestamp)
+    VALUES (:u, :r, :a, :o, :t)
+    """), {
+        "u": user,
+        "r": role,
+        "a": aksi,
+        "o": objek,
+        "t": datetime.now()
+    })
