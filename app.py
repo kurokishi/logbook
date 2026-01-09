404d265a4aa2060f13f44adbb929b1a00cdd9e53
kurokishi <kuroreidho@gmail.com>
2026-01-09 13:03:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-import os
 import streamlit as st
 from database import init_db, engine
 from auth import login
 from permissions import allow
 from modules import tiket, approval, ekinerja
 
-os.makedirs("uploads", exist_ok=True)
+st.set_page_config("SIM-IT RSUD (Offline)", layout="wide")
 
-st.set_page_config("SIM-IT RSUD", layout="wide")
-
-init_db()  # AUTO CREATE DB + ADMIN
+init_db()
 
 if "user" not in st.session_state:
     login(engine)
