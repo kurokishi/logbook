f2c9e3d5d03e0bbaae3737021b66479176ae9a57
kurokishi <kuroreidho@gmail.com>
2026-01-09 11:21:58 +0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,28 +1,29 @@
 import streamlit as st
-from sqlalchemy import text
 import hashlib
+from sqlalchemy import text
 
 def hash_pw(pw):
     return hashlib.sha256(pw.encode()).hexdigest()
 
 def login(engine):
     st.sidebar.subheader("Login")
-    user = st.sidebar.text_input("Username")
-    pw = st.sidebar.text_input("Password", type="password")
+    u = st.sidebar.text_input("Username")
+    p = st.sidebar.text_input("Password", type="password")
 
     if st.sidebar.button("Login"):
         q = text("""
-        SELECT username, role FROM users
-        WHERE username=:u AND password_hash=:p AND aktif=1
+        SELECT username, nama, role FROM users
+        WHERE username=:u AND password=:p AND aktif=1
         """)
         r = engine.execute(q, {
-            "u": user,
-            "p": hash_pw(pw)
+            "u": u,
+            "p": hash_pw(p)
         }).fetchone()
 
         if r:
             st.session_state.user = r[0]
-            st.session_state.role = r[1]
-            st.success("Login berhasil")
+            st.session_state.nama = r[1]
+            st.session_state.role = r[2]
+            st.sidebar.success("Login berhasil")
         else:
-            st.error("Login gagal")
+            st.sidebar.error("Login gagal")
