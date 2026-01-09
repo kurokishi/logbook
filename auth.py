f6c367e2d465d171b455eeb0f0e7fd012b3088c6
kurokishi <kuroreidho@gmail.com>
2026-01-09 11:45:24 +0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -6,13 +6,13 @@ def hash_pw(pw):
     return hashlib.sha256(pw.encode()).hexdigest()
 
 def login(engine):
-    st.sidebar.subheader("Login")
+    st.sidebar.subheader("Login SIM-IT RSUD")
     u = st.sidebar.text_input("Username")
     p = st.sidebar.text_input("Password", type="password")
 
     if st.sidebar.button("Login"):
         q = text("""
-        SELECT username, nama, role FROM users
+        SELECT username,nama,role FROM users
         WHERE username=:u AND password=:p AND aktif=1
         """)
         r = engine.execute(q, {
@@ -24,6 +24,6 @@ def login(engine):
             st.session_state.user = r[0]
             st.session_state.nama = r[1]
             st.session_state.role = r[2]
-            st.sidebar.success("Login berhasil")
+            st.success("Login berhasil")
         else:
-            st.sidebar.error("Login gagal")
+            st.error("Login gagal")
