4d0171508690e0011203a599c3e6661ecbd47676
kurokishi <kuroreidho@gmail.com>
2026-01-09 11:22:20 +0700

    Update error message for access denial

--- permissions.py
+++ permissions.py
@@ -1,6 +1,9 @@
+import streamlit as st
+
 def allow(*roles):
     if "role" not in st.session_state:
         st.stop()
     if st.session_state.role not in roles:
-        st.error("Anda tidak memiliki akses")
+        st.error("Akses ditolak")
         st.stop()
+
