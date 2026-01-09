66d419796d24a757ed06e70579be5660fa8af675
kurokishi <kuroreidho@gmail.com>
2026-01-09 11:53:49 +0700

    Update permissions.py

--- permissions.py
+++ permissions.py
@@ -1,8 +1,6 @@
 import streamlit as st
 
 def allow(*roles):
-    if "role" not in st.session_state:
-        st.stop()
     if st.session_state.role not in roles:
         st.error("Akses ditolak")
         st.stop()
