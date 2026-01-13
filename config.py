ec3843b16007abec505013e834329e36476fd4dd
kurokishi <kuroreidho@gmail.com>
2026-01-13 08:11:27 +0700

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-BOBOT_ANGKA_KREDIT = {
-    "Perbaikan Printer": 0.20,
-    "Troubleshooting Jaringan": 0.30,
-    "Perbaikan Komputer": 0.25,
-    "SIMRS / Aplikasi": 0.40,
-    "Scanner / Periferal": 0.15
-}
