e6e9925f9c3754d87ffd5c002e178f30d39c19d5
kurokishi <kuroreidho@gmail.com>
2026-01-13 10:25:39 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from routers import dashboard
+from export import router as export_router
 
 
 app = FastAPI(title="RSUD Ticketing System")
 
 
 app.include_router(dashboard.router, prefix="/api")
+app.include_router(export_router)
