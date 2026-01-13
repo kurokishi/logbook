2f2469dca45f791444e25d7290950c73236fc105
kurokishi <kuroreidho@gmail.com>
2026-01-13 11:31:25 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import DashboardKabag from './pages/DashboardKabag'
+import DashboardDirektur from './pages/DashboardDirektur'
 
 
 export default function App() {
@@ -7,6 +8,7 @@ return (
 <BrowserRouter>
 <Routes>
 <Route path="/kabag" element={<DashboardKabag />} />
+<Route path="/direktur" element={<DashboardDirektur />} />
 <Route path="*" element={<Navigate to="/kabag" />} />
 </Routes>
 </BrowserRouter>
