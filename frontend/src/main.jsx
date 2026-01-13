df77df7b5a796380b186454fb91158df1da3150f
kurokishi <kuroreidho@gmail.com>
2026-01-13 11:47:42 +0700

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './App.css'
-
+from routers.report_archive import router as report_router
+app.include_router(report_router)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <React.StrictMode>
